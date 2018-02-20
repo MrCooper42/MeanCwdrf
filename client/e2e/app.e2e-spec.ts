8cc719430d51f9829a2a9fd250a33077211eb7f3
MrCooper42 <mr.cooper42co@gmail.com>
2018-02-20 14:20:34 -0700

    Update between computers

--- client/e2e/app.e2e-spec.ts
+++ client/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
-import { AppPage } from './app.po';
+import {AppPage} from './app.po';
 
-describe('untitled App', () => {
+describe('CWDRF App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- karma.conf.js
+++ karma.conf.js
@@ -3,8 +3,8 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
+    basePath: './client/',
+    frameworks: [ 'jasmine', '@angular/cli' ],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
@@ -12,7 +12,7 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
@@ -22,12 +22,12 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: [ 'progress', 'kjhtml' ],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: [ 'Chrome' ],
     singleRun: false
   });
 };
