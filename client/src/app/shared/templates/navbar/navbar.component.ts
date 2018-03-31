07df38058d5a9f38adb7a1ecf4c72bc1f36fb4b2
MrCooper42 <mr.cooper42co@gmail.com>
2018-03-31 08:21:08 -0600

    updating pics

--- client/src/app/shared/templates/navbar/navbar.component.ts
+++ client/src/app/shared/templates/navbar/navbar.component.ts
@@ -6,21 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [ './navbar.component.css' ]
 })
 export class NavbarComponent implements OnInit {
+  private CWDRFLogo = '../assets/images/CDRF_Logo_2018.png';
   public show: boolean;
   public lgRightOptions: string;
   public smRightOptions: string;
+  public toggleShow = (): void => {
+    this.show = !this.show;
+  }
+  public getNavSize = (): string => this.show ? this.lgRightOptions : this.smRightOptions;
 
   constructor() {
     this.show = true;
     this.lgRightOptions = 'navbar-nav flex-row ml-md-auto d-none d-md-flex';
     this.smRightOptions = 'navbar-nav';
   }
-
-  public toggleShow = (): void => {
-    this.show = !this.show;
-  }
-
-  public getNavSize = (): string => this.show ? this.lgRightOptions : this.smRightOptions;
 
   ngOnInit() {
   }
